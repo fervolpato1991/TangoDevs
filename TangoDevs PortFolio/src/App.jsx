68bffad5e8445bc7fce6bb13564bcbd5cf5df579
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2023-08-01 12:59:20 -0300

    Language

--- TangoDevs PortFolio/src/App.jsx
+++ TangoDevs PortFolio/src/App.jsx
@@ -1,8 +1,21 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import { useState } from 'react';
 
-function App() {
+function App() {  
+  const localLanguage= navigator.language.toLowerCase()
+  const [language, setLanguage] = useState(localLanguage.startsWith('es')? 'es': 'en')
+
+  console.log(language);
+
+  const changeLanguageEN=()=>{
+    if(language !== 'en') setLanguage('en')
+  }
+  const changeLanguageES=()=>{
+    if (language !== 'es') setLanguage('es')
+  }
+
   return (
     <div>
       <Routes>
