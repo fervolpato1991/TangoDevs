9e597575df07e2076e446fc02c93fc82560cac2f
GgKallisti <riptheraven@gmail.com>
2023-08-03 15:30:53 -0300

    coso

--- TangoDevs PortFolio/src/components/Layout/Layout.jsx
+++ TangoDevs PortFolio/src/components/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import './Layout.scss';
+<<<<<<< Updated upstream
 import SideBar from '../SideBar/SideBar';
 
 const Layout = () => {
@@ -6,5 +7,27 @@ const Layout = () => {
         <SideBar/>
     )
 }
+=======
+import Sidebar from '../SideBar';
+
+const Layout = () => {
+    return (
+<div className='app'>
+   <Sidebar/>
+   <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
+        
+        )}
+
+>>>>>>> Stashed changes
 
 export default Layout;
