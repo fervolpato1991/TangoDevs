43d3dfc82748b0fb927602a4298fc5c1ed431776
GgKallisti <riptheraven@gmail.com>
2023-08-09 23:28:17 -0300

    fixed about, contact sidebar n stuff

--- TangoDevs PortFolio/src/components/Contact/index.jsx
+++ TangoDevs PortFolio/src/components/Contact/index.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
   const form = useRef()
 
   useEffect(() => {
-    return setTimeout(() => {
+    setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
@@ -49,7 +49,7 @@ const Contact = () => {
             questions, don't hesitate to contact me using below form either.
           </p>
           <div className="contact-form">
-            <form ref={form} onSubmit={sendEmail}>
+            <form ref={form} >
               <ul>
                 <li className="half">
                   <input placeholder="Name" type="text" name="name" required />
@@ -85,22 +85,14 @@ const Contact = () => {
           </div>
         </div>
         <div className="info-map">
-          Slobodan Gajić,
+         Tango Devs
           <br />
-          Serbia,
-          <br />
-          Branka RadiČevića 19, 22000 <br />
-          Sremska Mitrovica <br />
-          <br />
-          <span>freelancerslobodan@gmail.com</span>
+    Argentina 
+         <br />
+          <span>arg.tangodevs@gmail.com</span>
         </div>
         <div className="map-wrap">
-          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[44.96366, 19.61045]}>
-              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
-            </Marker>
-          </MapContainer>
+         
         </div>
       </div>
       <Loader type="pacman" />
--- TangoDevs PortFolio/src/components/SideBar/index.jsx
+++ TangoDevs PortFolio/src/components/SideBar/index.jsx
@@ -1,12 +1,49 @@
 import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import logo from '../../assets/images/logo-s.png'
- 
+import { useState, useEffect } from 'react'; 
+import {
+    faLinkedin,
+    faGithub,
+    faYoutube,
+    faSkype,
+  } from '@fortawesome/free-brands-svg-icons'
+  import {
+    faHome,
+    faUser,
+    faEnvelope,
+    faSuitcase,
+    faBars,
+    faClose,
+  } from '@fortawesome/free-solid-svg-icons'
+
 const SideBar = () => {
+    const [scrolled, setScrolled] = useState(false);
+    const [showNav, setShowNav] = useState(false);
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
+  
     return (
-        <div className='navBar'>
+        <div className={`${
+            scrolled ? "scrolled" : "navBar"
+          }`} >
             <Link to='/' className='logo'>
                 <img src={logo} alt="TangoDevsLogo"/>
             </Link>
@@ -14,12 +51,18 @@ const SideBar = () => {
                 <NavLink exact="true" activeclassname="active" to="/">
                     <FontAwesomeIcon icon={faHome} color="4d4d4e"/>
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="about" to="/about">
-                    <FontAwesomeIcon icon={faUser} color="4d4d4e"/>
+                <NavLink  activeclassname="active" className="portfolio-link" to="/projects"
+                    onClick={() => setShowNav(false)} >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="contact" to="/contact">
-                    <FontAwesomeIcon icon={faEnvelope} color="4d4d4e"/>
+                <NavLink activeclassname="active" className="about-link" to="/about"
+                onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
                 </NavLink>
+                <NavLink  activeclassname="active" className="contact-link" to="/contact"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
             </nav>
         </div>
     )
